b199d7cb0423bbc747d513c0a843aebf65702bd3
Reysmer Valle <reysmerwvr@gmail.com>
2018-03-08 11:06:09 -0400

    Adding Confirmation Modal + Employee CRUD with Firebase

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -3,7 +3,8 @@ import { Actions } from 'react-native-router-flux';
 import {
     EMPLOYEE_UPDATE,
     EMPLOYEE_CREATE,
-    EMPLOYEES_FETCH_SUCCESS
+    EMPLOYEES_FETCH_SUCCESS,
+    EMPLOYEE_SAVE_SUCCESS
 } from './types';
 
 export const employeeUpdate = ({ prop, value }) => {
@@ -34,6 +35,31 @@ export const employeesFetch = () => {
         .on('value', snapshot => {
             dispatch({ type: EMPLOYEES_FETCH_SUCCESS, payload: snapshot.val() });
         });
+    };
+};
+
+export const employeeSave = ({ name, phone, shift, uid }) => {
+    const { currentUser } = firebase.auth();
+    // backtips ``
+    return (dispatch) => {
+        firebase.database().ref(`/users/${currentUser.uid}/employees/${uid}`)
+        .set({ name, phone, shift })
+        .then(() => {
+            dispatch({ type: EMPLOYEE_SAVE_SUCCESS });
+            Actions.main().employeeList({ type: 'reset' });
+        });        
+    };
+};
+
+export const employeeDelete = ({ uid }) => {
+    const { currentUser } = firebase.auth();
+    // backtips ``
+    return () => {
+        firebase.database().ref(`/users/${currentUser.uid}/employees/${uid}`)
+        .remove()
+        .then(() => {
+            Actions.main().employeeList({ type: 'reset' });
+        });        
     };
 };
 
--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Picker, Text, StyleSheet } from 'react-native';
-import { Card, CardSection, Input, Button } from './common';
+import { Card, CardSection, Button } from './common';
 import { employeeUpdate, employeeCreate } from '../actions';
+import EmployeeForm from './EmployeeForm';
 
 class EmployeeCreate extends Component {
 	
@@ -15,41 +15,7 @@ class EmployeeCreate extends Component {
 	render() {
         return (
             <Card>
-				<CardSection>
-					<Input
-						label="Name"
-						placeholder="Jane"
-						value={this.props.name}
-						onChangeText={value => this.props.employeeUpdate({ prop: 'name', value })}
-					/>
-				</CardSection>
-
-				<CardSection>
-					<Input
-						label="Phone"
-						placeholder="555-555-5555"
-						value={this.props.phone}
-						onChangeText={value => this.props.employeeUpdate({ prop: 'phone', value })}
-					/>
-				</CardSection>
-
-				<CardSection>
-					<Text style={styles.pickerTextStyle}>Shift</Text>
-					<Picker
-						style={{ flex: 1 }}
-						selectedValue={this.props.shift}
-						onValueChange={value => this.props.employeeUpdate({ prop: 'shift', value })}
-					>
-						<Picker.Item label="Monday" value="Monday" />
-						<Picker.Item label="Tuesday" value="Tuesday" />
-						<Picker.Item label="Wednesday" value="Wednesday" />
-						<Picker.Item label="Thursday" value="Thursday" />
-						<Picker.Item label="Friday" value="Friday" />
-						<Picker.Item label="Saturday" value="Saturday" />
-						<Picker.Item label="Sunday" value="Sunday" />
-					</Picker>
-				</CardSection>
-
+				<EmployeeForm {...this.props} />
 				<CardSection>
 					<Button onPress={this.onButtonPress.bind(this)}>
 						Create
@@ -64,13 +30,6 @@ const mapStateToProps = (state) => {
 	const { name, phone, shift } = state.employeeForm;
 	return { name, phone, shift };
 };
-
-const styles = StyleSheet.create({
-	pickerTextStyle: {
-		fontSize: 18,
-		paddingLeft: 20,
-	},
-});
 
 export default connect(mapStateToProps, {
 	employeeUpdate, employeeCreate 
